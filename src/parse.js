7cbe88fcce35ce658963da5c2d7664aadc0a40d8
pustovitDmytro <pustic@gmail.com>
2018-01-18 01:01:48 +0200

    onclose added

--- src/parse.js
+++ src/parse.js
@@ -4,12 +4,12 @@ const rl = readline.createInterface({
     input : process.stdin,
     output: process.stdout
 });
-var tree = {};
-var lookup = {};
+const tree = {};
+const lookup = {};
 let i = 1;
 let parent = 0;
 
-const omit = ({children, version}) => ({children, version});
+const omit = ({ children, version }) => ({ children, version });
 
 rl.on('line', (input) => {
     const reStucture = /^(\W+)\s/g;
@@ -18,28 +18,21 @@ rl.on('line', (input) => {
     const reIsSimple = /─/g;
 
     const bundle = {};
-    let root = bundle;
+    const root = bundle;
     const reModule = /\s*([^\s]+)@([^\s]+)/g;
     const map = {};
-    const [ , structure ] = reStucture.exec(input) || ['', '┬'];
+    const [, structure] = reStucture.exec(input) || ['', '┬'];
     // console.log('structure', structure);
-    const isLast = !!~structure.search(reIsLast);
-    const isSimple = !!~structure.search(reIsSimple);
-    const hasChild = !!~structure.search(reHasChild);
-    let [, moduleName, version] = reModule.exec(input)||['', 'root', ''];
+    const isLast = Boolean(~structure.search(reIsLast));
+    const isSimple = Boolean(~structure.search(reIsSimple));
+    const hasChild = Boolean(~structure.search(reHasChild));
+    const [, moduleName, version] = reModule.exec(input)||['', 'root', ''];
 
-    // const element = {version};
-    // map[moduleName] = {link: element};
+    const obj = { id: i, parent_id: parent, name: moduleName, children: {}, version };
 
-    // root[moduleName] = element;
-    // const parent  = root;
-    // if (hasChild) element.childs = {};
-    //
-    // if (isLast) root = parent;
-    // console.log({isLast, isSimple, hasChild, moduleName, version});
-    var obj = { id: i, parent_id: parent, name: moduleName, children: {}, version };
-
-    if (hasChild) parent = i;
+    if (hasChild) {
+        parent = i;
+    }
     lookup[obj.id] = obj;
 
     if (lookup[obj.parent_id]) {
@@ -47,9 +40,9 @@ rl.on('line', (input) => {
     } else {
         tree[obj.name] = omit(obj);
     }
-
-    // console.log(i, moduleName);
-    // console.log(omit(obj));
     i++;
-    if(i==22) console.log(JSON.stringify(tree));
+});
+
+rl.on('close', () => {
+    console.log(JSON.stringify(tree))
 });

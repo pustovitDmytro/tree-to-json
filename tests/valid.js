fb40b19e23c6d8fed15a6f91405dd9a935632220
pustovitDmytro <pustic@gmail.com>
2018-01-19 23:16:48 +0200

    passing valid tests

--- tests/valid.js
+++ tests/valid.js
@@ -2,21 +2,34 @@ require('babel-polyfill');
 const fs = require('fs');
 import { assert } from 'chai';
 const path = require('path');
-
-const inputPath = path.resolve(__dirname, 'short.list');
-const outputPath = path.resolve(__dirname, 'tmp/short.json');
-
-const input = fs.createReadStream(inputPath);
-const output = fs.createWriteStream(outputPath);
 const { parseAsync, parse } = require("../src/index.js");
 
 suite('VALID JSON');
 
-test('Short File', async () => {
+test('Positive: Short File', async () => {
+    const inputPath = path.resolve(__dirname, 'lists/short.list');
+    const outputPath = path.resolve(__dirname, 'tmp/short.json');
+    const input = fs.createReadStream(inputPath);
+    const output = fs.createWriteStream(outputPath);
+
     await parseAsync({ input, output });
     const json = require(outputPath);
 });
 
-test('Short File: nodeback', () => {
+test('Positive: Short File: nodeback', () => {
+    const inputPath = path.resolve(__dirname, 'lists/short.list');
+    const outputPath = path.resolve(__dirname, 'tmp/short.json');
+    const input = fs.createReadStream(inputPath);
+    const output = fs.createWriteStream(outputPath);
+
     parse({ input, output }, (err, done) => JSON.parse(JSON.stringify(done)));
+});
+
+test('Positive: Long File: Async', async () => {
+    const inputPath = path.resolve(__dirname, 'lists/long.list');
+    const outputPath = path.resolve(__dirname, 'tmp/long.json');
+    const input = fs.createReadStream(inputPath);
+    const output = fs.createWriteStream(outputPath);
+
+    await parseAsync({ input, output });
 });

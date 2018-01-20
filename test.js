fa42c82727d449f7e2157406d0ef2e00afc8f03c
pustovitDmytro <pustic@gmail.com>
2018-01-21 01:23:38 +0200

    rimraf added

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
 const dir = path.resolve(__dirname, 'tests/tmp');
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (fs.existsSync(dir)){
+    rimraf.sync(dir);
 }
+fs.mkdirSync(dir);
 
 const { execSync } = require('child_process');
 execSync('npm run build');
